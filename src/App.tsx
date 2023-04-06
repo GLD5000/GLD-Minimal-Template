ecf3564bd80e348195178585f6591eb20f41723f
GLD5000 <garethlouisdevlin@gmail.com>
2023-04-07 00:00:04 +0100

    Removed unused content

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,14 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
-function App(): JSX.Element {
-    const [count, setCount] = useState(0);
-
+export default function App(): JSX.Element {
     return (
-        <div className="App">
-            <div>
-                <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a className='ml-auto w-2' href="https://reactjs.org" target="_blank" rel="noreferrer">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1 className='text-xs'>Vite + React</h1>
-            <div className="card">
-                <button
-                    type="button"
-                    onClick={() => {
-                        setCount((value) => value + 1);
-                    }}
-                >
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+        <div className="w-screen h-screen p-20">
+            <h1 className="mx-auto w-fit text-4xl">GLD Minimal Template</h1>
+            <ul className=" list-disc mt-10 mx-auto w-fit">
+                <li>Vite & SWC</li>
+                <li>React & TypeScript</li>
+                <li>TailwindCSS</li>
+                <li>Prettier & Eslint (with Airbnb style)</li>
+                <li>Husky & Lint-Staged</li>
+            </ul>
         </div>
     );
 }
-
-export default App;
